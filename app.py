3578884dc15cfa5416d01d71e5e27169bced57d4
hamed <hamed.zoghi@gmail.com>
2022-05-22 23:53:09 -0400

    updated app, requirements and models

--- app.py
+++ app.py
@@ -3,21 +3,54 @@
 from flask import Flask
 from flask_migrate import Migrate
 from flask_cors import CORS
-from models import db_drop_and_create_all, setup_db
+from flask_sqlalchemy import SQLAlchemy
+from flask_moment import Moment
 
 # app initiation and database setup
 # -------------------------------------------------------- #
 app = Flask(__name__)
-migrate = Migrate()
-CORS(app)
+# CORS(app)
+# app.config.from_object('config')
+# moment = Moment(app)
+# db = SQLAlchemy(app)
+#
+# migrate = Migrate(app. db)
 
-setup_db(app)
-db_drop_and_create_all()
 
-#endpoints
+def setup_db(app):
+    """
+    to setup database parameters
+    :param app: Flask object
+    :return: none
+    """
+    app.config.from_object('config')
+    db.app = app
+    db.init_app(app)
+
+
+def db_drop_and_create_all():
+    """
+    recreate the database
+    :return: None
+    """
+    db.drop_all()
+    db.create_all()
+
+
+# setup_db(app)
+# db_drop_and_create_all()
+
+# endpoints
 # -------------------------------------------------------- #
 
 # TODO::: Implement GET/actors and /movies
 # TODO::: Implement DELETE/actors and /movies
 # TODO::: Implement POST/actors and /movies
 # TODO::: Implement PATCH /actors and /movies
+
+@app.route('/')
+def test():
+    return 'hello'
+
+if __name__ == '__main__':
+    app.run()
--- models.py
+++ models.py
@@ -1,38 +1,20 @@
 # Data Models
 # Imports
-from flask_sqlalchemy import SQLAlchemy
-
+from app import db
+# from flask_sqlalchemy import SQLAlchemy
 # -------------------------------------------------------- #
-db = SQLAlchemy()
 
 
 # utilities
 # -------------------------------------------------------- #
-def setup_db(app):
-    """
-    to setup database parameters
-    :param app: Flask object
-    :return: none
-    """
-    app.config.from_object('config')
-    db.app = app
-    db.init_app(app)
 
-
-def db_drop_and_create_all():
-    """
-    recreate the database
-    :return: None
-    """
-    db.drop_all()
-    db.create_all()
 
 
 # tables
 # -------------------------------------------------------- #
 association_table = db.Table('association',
-                             db.Column('movie_id', db.ForeignKey('movie.id')),
-                             db.Column('actor_id', db.ForeignKey('actor.id')))
+                             db.Column('movie_id', db.ForeignKey('movie.id'), primary_key=True),
+                             db.Column('actor_id', db.ForeignKey('actor.id'), primary_key=True))
 
 
 class Movie(db.Model):
